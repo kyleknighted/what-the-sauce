dab1461391c9badc72b26fc34c329a2cffbacbfe
idpro <kyle@idprojections.com>
2012-07-25 08:42:27 -0500

    Adding standard Rails file structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhatTheSauce::Application.config.secret_token = '7f20561d020dbf2b37f8b8e8239fa647685dff2928cccb977195ac4ea71e7e3967425593fe57c3409b3cf9ad26482c81ab1d8e2c00fc02498e9a6ce7a5fe8ba5'
